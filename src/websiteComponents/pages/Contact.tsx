ba6b3c630590cdba163a95342e220921c5be5f58
Aimal Bazai <ak9971259@gmail.com>
2023-09-21 19:09:28 +0500

    mail link

--- src/websiteComponents/pages/Contact.tsx
+++ src/websiteComponents/pages/Contact.tsx
@@ -21,9 +21,7 @@ const ContactPage: React.FC = () => {
     e.preventDefault();
     try {
       await axios.get(
-        `https://constraction-app.vercel.app/SendEmail?receiverEmail=ak9971259@gmail.com&subject=Assistance
-
-`
+        `/SendEmail?receiverEmail=ak9971259@gmail.com&subject=Assistance`
       );
 
       alert("Email sent successfully!");
